a81812c5bcdedae66c775263fce88c883eb26937
orion55 <grol55@mail.ru>
2019-12-17 15:54:59 +0500

    Новая версия проверки уведомлений

---
+++ NoticeCB2/noticeCB2.ps1
@@ -0,0 +1,141 @@
+﻿#Программа проверка извещение из ЦБ (версия 2)
+#от 17.12.2019
+
+#текущий путь
+$curDir = Split-Path -Path $myInvocation.MyCommand.Path -Parent
+
+[string]$lib = "$curDir\lib"
+. $curDir/variables.ps1
+. $lib/PSMultiLog.ps1
+. $lib/libs.ps1
+
+Set-Location $curDir
+
+Clear-Host
+
+Start-HostLog -LogLevel Information
+Start-FileLog -LogLevel Information -FilePath $logName -Append
+
+testDir(@($noticePath))
+createDir(@($logPath))
+function 440Handler {
+	Param($file)
+
+	[hashtable]$return = @{ }
+	$return.flagErr = $false
+	$return.bodyMail = ''
+	$return.type = '440'
+
+	$tmpFile = $file.FullName + '.test'
+	$arguments = "-verify -delete -1 -profile $profile -registry -in $($file.FullName) -out $tmpFile -silent $logSpki"
+	Write-Log -EntryType Information -Message "Обрабатываем файл $($file.Name)"
+	Start-Process $spki $arguments -NoNewWindow -Wait
+
+	$msg = Remove-Item $($file.FullName) -Verbose -Force *>&1
+	Write-Log -EntryType Information -Message ($msg | Out-String)
+	$msg = Get-ChildItem $tmpFile | Rename-Item -NewName { $_.Name -replace '.test$', '' } -Verbose *>&1
+	Write-Log -EntryType Information -Message ($msg | Out-String)
+
+	$file = Get-ChildItem "$noticePath\$file"
+	[xml]$xmlOutput = Get-Content $file
+
+	$xmlTag = $xmlOutput.Файл.ИЗВЦБКОНТР
+	if ($xmlTag.КодРезПроверки -ne "01") {
+		$return.flagErr = $true
+	}
+	$msg = 'ИмяФайла: ' + $xmlTag.ИмяФайла + ' Результат: ' + $xmlTag.Пояснение
+	$return.bodyMail += $msg + "`r`n"
+
+	if ($xmlTag.КодРезПроверки -ne "01") {
+		Write-Log -EntryType Error -Message $msg
+	}
+	else {
+		Write-Log -EntryType Information -Message $msg
+	}
+
+	$newName = $file.BaseName + '~' + $file.Extension
+	$msg = Rename-Item $file -NewName $newName -Verbose -Force *>&1
+	Write-Log -EntryType Information -Message ($msg | Out-String)
+
+	return $return
+}
+function 311Handler {
+	Param($file)
+
+	[hashtable]$return = @{ }
+	$return.flagErr = $false
+	$return.bodyMail = ''
+	$return.type = '311'
+
+	[xml]$content = Get-Content $($file.FullName)
+	$rezArh = $content.UV.REZ_ARH
+
+	$msg = 'ИмяФайла: ' + $content.UV.ARH + ' Результат: ' + $rezArh
+	$return.bodyMail += $msg + "`r`n"
+
+	if ($rezArh -notlike "принят") {
+		$return.flagErr = $true
+	}
+
+	if ($return.flagErr) {
+		Write-Log -EntryType Error -Message $msg
+	}
+	else {
+		Write-Log -EntryType Information -Message $msg
+	}
+
+	$newName = $file.BaseName + '~' + $file.Extension
+	$msg = Rename-Item $($file.FullName) -NewName $newName -Verbose -Force *>&1
+	Write-Log -EntryType Information -Message ($msg | Out-String)
+
+	return $return
+}
+
+function sendEmail {
+	Param([string]$title, [string]$body, [string]$mailAddr)
+	Write-Log -EntryType Information -Message "Отправка почтового сообщения"
+	if (Test-Connection $mailServer -Quiet -Count 2) {
+		$encoding = [System.Text.Encoding]::UTF8
+		Send-MailMessage -To $mailAddr -Body $body -Encoding $encoding -From $mailFrom -Subject $title -SmtpServer $mailServer
+	}
+	else {
+		Write-Log -EntryType Error -Message "Не удалось соединиться с почтовым сервером $mailServer"
+	}
+}
+
+[boolean]$debug = $true
+if ($debug) {
+	Remove-Item -Path "$noticePath\*.*"
+	Copy-Item -Path "$curDir\OUT1\*.*" -Destination $noticePath
+}
+
+$findFiles = Get-ChildItem -Path $noticePath | Where-Object { ! $_.PSIsContainer } | Where-Object { $_.Name -match $400Mask -or $_.Name -match $311Mask }
+$count = ($findFiles | Measure-Object).count
+
+if ($count -eq 0) {
+	exit
+}
+Write-Log -EntryType Information -Message "Начинаем обработку..."
+ForEach ($file in $findFiles) {
+	if ($file -match $400Mask) {
+		$result = 440Handler -file $file
+	}
+	if ($file -match $311Mask ) {
+		$result = 311Handler -file $file
+		if ($file -match $311MaskFiz) {
+			$result.type = '311-Физ'
+		}
+		elseif ($file -match $311MaskJur) {
+			$result.type = '311-Юр'
+		}
+	}
+
+	$title = "Извещение о проверке файла сообщения по форме " + $result.type
+	if ($result.flagErr){
+		$title = 'Ошибка! ' + $title
+	}
+	#sendEmail -title
+}
+
+Stop-FileLog
+Stop-HostLog
---
+++ NoticeCB2/variables.ps1
@@ -0,0 +1,35 @@
+﻿[string]$curDir = Split-Path -Path $myInvocation.MyCommand.Path -Parent
+
+#каталог с извещениями
+[string]$noticePath = "$curDir\OUT"
+
+#рабочий временный каталог
+[string]$tmp = "$curDir\temp"
+
+#настройка почты
+[string]$440mailAddr = "tmn-goe@tmn.apkbank.ru"
+#[string]$440mailAddr = "tmn-f365@tmn.apkbank.apk"
+[string]$311mailAddrFiz = "tmn-goe@tmn.apkbank.ru"
+#[string]$311mailAddrFiz = "tmn-f311@tmn.apkbank.apk"
+[string[]]$311mailAddrJur = "<tmn-goe@tmn.apkbank.ru>", "<lma@tmn.apkbank.ru>"
+#[string[]]$311mailAddrJur = "<tmn-lov@tmn.apkbank.ru>", "<tmn_oit@tmn.apkbank.apk>"
+
+[string]$mailServer = "191.168.6.50"
+[string]$mailFrom = "atm_support@tmn.apkbank.apk"
+
+#входящие - настройки
+[string]$400Mask = "^IZVTUB_.+[^~]\.xml$"
+[string]$311Mask = "^UV.+[^~]\.xml$"
+[string]$311MaskFiz = "^UVBN.+[^~]\.xml$"
+[string]$311MaskJur = "^UVAN.+[^~]\.xml$"
+
+$curDate = Get-Date -Format "ddMMyyyy"
+[string]$logPath = "$curDir\log"
+#имя лог-файла
+[string]$logName = $logPath + '\' + $curDate +"_notice.log"
+
+[string]$spki = "C:\Program Files\MDPREI\spki\spki1utl.exe"
+[string]$vdkeys = "d:\SKAD\Floppy\foiv"
+[string]$profile = "r2880_2"
+[string]$logSpki = $curDir + "\log\" + $curDate + "_spki_tr.log"
+#[string]$recList = "$curDir\util\Reclist.conf"
