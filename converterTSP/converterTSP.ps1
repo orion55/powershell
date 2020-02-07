ecdade7f47683f2850f067b93a0b0909ebd95e43
orion55 <grol55@mail.ru>
2020-02-07 16:04:44 +0500

    converterTSP

---
+++ converterTSP/converterTSP.ps1
@@ -0,0 +1,15 @@
+﻿#Программа для разбиения Excel-файла на другие с фильтрации
+#(c) Гребенёв О.Е. 7.02.2020
+
+[string]$curDir = Split-Path -Path $myInvocation.MyCommand.Path -Parent
+Set-Location $curDir
+[string]$lib = "$curDir\lib"
+
+. $curDir/variables.ps1
+. $lib/libs.ps1
+
+Clear-Host
+
+createDir(@($tmpPath, $outPath))
+Remove-Item "$tmpPath\*.*" -recurse | Where-Object { ! $_.PSIsContainer }
+testFiles(@($dostowin))
---
+++ converterTSP/variables.ps1
@@ -0,0 +1,14 @@
+$curDir = Split-Path -Path $myInvocation.MyCommand.Path -Parent
+
+[string]$inPath = "$curDir\in"
+[string]$tmpPath = "$curDir\tmp"
+[string]$outPath = "$curDir\out"
+
+[string]$dostowin = "$curDir\lib\dostowin.exe"
+
+$org = @{
+    'AO ERIC YANAO' = 'АО ЕРИЦ ЯНАО'
+    'OAO TRIC'      = 'ОАО ТРИЦ'
+    'LC.YRITZ'      = 'ООО ЮРИЦ'
+    'OOO NESKO'     = 'ООО НЭСКО'
+}
